4e20dc2b5818a2ad17b72ea9f53c17df80ca969b
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-27 00:48:24 +0530

    Create mood.py

---
+++ mood.py
@@ -0,0 +1,13 @@
+from database import Database
+
+db = Database()
+
+def add_mood(user_id, emoji):
+    db.add_mood(user_id, emoji)
+
+def get_mood_stats(user_id):
+    data = db.get_mood_stats(user_id)
+    stats = {}
+    for date, mood in data:
+        stats[date] = mood
+    return stats
