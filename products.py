d83dcc29174f51968adbed25051ab4f6a3c5c29d
Akshay Verma <133236174+akshayverma3685@users.noreply.github.com>
2025-08-27 00:47:59 +0530

    Create products.py

---
+++ products.py
@@ -0,0 +1,9 @@
+PRODUCTS = [
+    "🌿 Eco-friendly pads: https://www.example.com/pads",
+    "🥛 Menstrual cups: https://www.example.com/cups",
+    "🩸 Tampons: https://www.example.com/tampons",
+    "🧴 Organic wet wipes: https://www.example.com/wipes"
+]
+
+def get_product_suggestions():
+    return "\n".join(PRODUCTS)
